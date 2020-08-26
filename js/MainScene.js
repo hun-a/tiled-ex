47eb18dfe60e3f7696a472c78d208152cc70e369
hun-a <seunghun.chan@gmail.com>
2020-08-27 00:39:15 +0900

    Finally! Add the Tile Map!

--- js/MainScene.js
+++ js/MainScene.js
@@ -8,10 +8,14 @@ export default class MainScene extends Phaser.Scene {
 
   preload() {
     Player.preload(this);
+    this.load.image('tiles', 'assets/images/RPG Nature Tileset.png');
+    this.load.tilemapTiledJSON('map', 'assets/images/map.json');
   }
 
   create() {
-    console.log('create');
+    const map = this.make.tilemap({ key: 'map' });
+    const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32, 0, 0);
+    const layer1 = map.createStaticLayer('Tile Layer 1', tileset, 0, 0);
     this.player = new Player({ scene: this, x: 0, y: 0, texture: 'knight', frame: 'eliteknight_idle_1' });
     const testPlayer = new Player({ scene: this, x: 100, y: 100, texture: 'knight', frame: 'eliteknight_idle_1' });
   }
