2a98fceb216403e7ad859b4840f1a06916eb06a8
hun-a <seunghun.chan@gmail.com>
2020-08-27 00:55:30 +0900

    Add the Collider to the bound of background tiles

--- js/MainScene.js
+++ js/MainScene.js
@@ -17,8 +17,10 @@ export default class MainScene extends Phaser.Scene {
     const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32, 0, 0);
     const layer1 = map.createStaticLayer('Tile Layer 1', tileset, 0, 0);
     const layer2 = map.createStaticLayer('Tile Layer 2', tileset, 0, 0);
-    this.player = new Player({ scene: this, x: 0, y: 0, texture: 'knight', frame: 'eliteknight_idle_1' });
-    const testPlayer = new Player({ scene: this, x: 100, y: 100, texture: 'knight', frame: 'eliteknight_idle_1' });
+    layer1.setCollisionByProperty({ collides: true });
+    this.matter.world.convertTilemapLayer(layer1);
+    this.player = new Player({ scene: this, x: 100, y: 100, texture: 'knight', frame: 'eliteknight_idle_1' });
+    const testPlayer = new Player({ scene: this, x: 200, y: 200, texture: 'knight', frame: 'eliteknight_idle_1' });
   }
 
   update() {
