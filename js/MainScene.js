a51a427e9045e3ee88acbf1f8a6902652afc55f4
hun-a <seunghun.chan@gmail.com>
2020-08-28 00:27:10 +0900

    Add a new Layer

--- js/MainScene.js
+++ js/MainScene.js
@@ -18,8 +18,11 @@ export default class MainScene extends Phaser.Scene {
     const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32, 0, 0);
     const layer1 = map.createStaticLayer('Tile Layer 1', tileset, 0, 0);
     const layer2 = map.createStaticLayer('Tile Layer 2', tileset, 0, 0);
+    const layer3 = map.createStaticLayer('Tile Layer 3', tileset, 0, 0);
     layer1.setCollisionByProperty({ collides: true });
+    layer3.setCollisionByProperty({ collides: true });
     this.matter.world.convertTilemapLayer(layer1);
+    this.matter.world.convertTilemapLayer(layer3);
     this.player = new Knight({ scene: this, x: 100, y: 100, texture: 'knight', frame: 'eliteknight_idle_1' });
     const testPlayer = new Knight({ scene: this, x: 200, y: 200, texture: 'knight', frame: 'eliteknight_idle_1' });
   }
